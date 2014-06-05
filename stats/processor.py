eb9fd86cfe2ffcde9f6582b902de81192cbd0aae
Woofles <salty.cea.dog@gmail.com>
2014-06-04 22:01:00 -0500

    processor testing

---
+++ stats/processor.py
@@ -0,0 +1,11 @@
+from core.models import Player
+import csv
+
+f = open("playerid_list.csv", "r")
+
+reader = csv.reader(f)
+
+for row in reader:
+    if row[5] != "NULL" and row[4] != "NULL":
+        p = Player.objects.get(retro_id=row[5])
+        print p.last_name
