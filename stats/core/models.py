3e52ad979988b4d2a82af6fd80c6a861e5737a5f
Woofles <salty.cea.dog@gmail.com>
2014-06-07 16:43:59 -0500

    no message

--- stats/core/models.py
+++ stats/core/models.py
@@ -10,8 +10,8 @@ class Player(models.Model):
 	
 	given_name = models.CharField(max_length=60)
 
-	weight = models.IntegerField(max_value=3000)
-	height = models.IntegerField(max_value=300)
+	weight = models.IntegerField()
+	height = models.IntegerField()
 
 	bats = models.CharField(max_length=3)
 	throws = models.CharField(max_length=3)
@@ -23,16 +23,16 @@ class Player(models.Model):
 	bbref_id = models.CharField(max_length=12)
 	lahman_id = models.CharField(max_length=12)
 
-	birth_year = models.IntegerField(max_value=9999)
-	birth_month = models.IntegerField(max_value=12)
-	birth_day = models.IntegerField(max_value=31)
+	birth_year = models.IntegerField()
+	birth_month = models.IntegerField()
+	birth_day = models.IntegerField()
 	birth_country = models.CharField(max_length=30)
 	birth_state = models.CharField(max_length=30)
 	birth_city = models.CharField(max_length=30)
 
-	death_year = models.IntegerField(max_value=9999, null=True)
-	death_month = models.IntegerField(max_value=12, null=True)
-	death_day = models.IntegerField(max_value=31, null=True)
+	death_year = models.IntegerField(null=True)
+	death_month = models.IntegerField(null=True)
+	death_day = models.IntegerField(null=True)
 	death_country = models.CharField(max_length=30, null=True)
 	death_state = models.CharField(max_length=30, null=True)
 	death_city = models.CharField(max_length=30, null=True)
@@ -52,16 +52,36 @@ class BatterSeason(models.Model):
 	triples = models.IntegerField()
 	home_runs = models.IntegerField()
 
-	def _get_singles(self):
-    	return '%d - %d - %d - %d' % (self.hits, self.double, self.triple, self.home_runs)
-	singles = property(_get_singles)
+	@property
+	def singles(self):
+		return self.hits - self.double - self.triple - self.home_runs
+	# singles = property(_get_singles)
 	
 	rbis = models.IntegerField()
 	stolen_bases = models.IntegerField()
+
+	caught_stealing = models.IntegerField()
+
+	walks = models.IntegerField()
+	strike_outs = models.IntegerField()
+	battingavg = models.DecimalField(max_digits=4, decimal_places=3)
+	obp = models.DecimalField(max_digits=4, decimal_places=3)
+
+	@property
+	def slg(self):
+		return (self.singles + 2*self.doubles + 3*self.triples + 4*self.home_runs)/self.at_bats
+	# slg = property(_get_slg)
+	
+	@property
+	def ops(self):
+		return self.obp + self.slg
+	# ops = property(_get_ops)
+
+	salary = models.IntegerField()
+
 	# stolen_bases_2 = models.IntegerField()
 	# stolen_bases_3 = models.IntegerField()
 	# stolen_bases_4 = models.IntegerField()
-	caught_stealing = models.IntegerField()
 	# caught_stealing_2 = models.IntegerField()
 	# caught_stealing_3 = models.IntegerField()
 	# caught_stealing_4 = models.IntegerField()
@@ -82,18 +102,6 @@ class BatterSeason(models.Model):
 	# second_single_third = models.IntegerField()
 	# second_single_home = models.IntegerField()
 	# bases_taken = models.IntegerField()
-	walks = models.IntegerField()
-	strike_outs = models.IntegerField()
-	battingavg = models.DecimalField(max_digits=4, decimal_places=3)
-	obp = models.DecimalField(max_digits=4, decimal_places=3)
-
-	def _get_slg(self):
-		return '(%d + 2*%d + 3*%d + 4*%d)/%d' % (self.singles, self.doubles, self.triples, self.home_runs, self.at_bats)
-    slg = property(_get_slg)
-	
-	def _get_ops(self):
-    	return '%d + %d' % (self.obp, self.slg)
-	ops = property(_get_ops)
 	# ops_plus = models.IntegerField()
 	# total_bases = models.IntegerField()
 	# gdp = models.IntegerField()
@@ -116,9 +124,23 @@ class BatterSeason(models.Model):
 	# owar = models.DecimalField(max_digits=2, decimal_places=1)
 	# dwar = models.DecimalField(max_digits=2, decimal_places=1)
 	# orar = models.DecimalField(max_digits=2, decimal_places=1)
-	salary = models.IntegerField()
 
+class Team(models.Model):
+	retro_id = models.CharField(primary_key=True,max_length=5)
+	lahman_id = models.CharField(max_length=5)
 
+	lg_id = models.CharField(max_length=3)
+	dv_id = models.CharField(max_length=3)
+
+	team_city = models.CharField(max_length=20)
+	team_name = models.CharField(max_length=20)
+	team_nickname = models.CharField(max_length=20)
+
+	first_year = models.DateField()
+	last_year = models.DateField(null=True)
+
+	loc_city = models.CharField(max_length=20)
+	loc_state = models.CharField(max_length=20)
 
 # class Pitcher(models.Model):
 # 	pitcherid = models.AutoField(primary_key=True)
